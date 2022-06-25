146e5b56f5312cf20d7a4cdf270342f29bcfd237
lucas-guimaraes <lucaslgr1206@gmail.com>
2022-06-25 18:14:23 -0300

    Split in functions and get the most occurrence of faces in a time window

--- exemplo1.py
+++ exemplo1.py
@@ -2,49 +2,63 @@ import cv2
 import time
 import requests
 
-xml_haar_cascade = 'haarcascade_frontalface_alt2.xml'
+XML_HAAR_CASCADE = 'haarcascade_frontalface_alt2.xml'
 
-faceClassifier = cv2.CascadeClassifier(xml_haar_cascade)
+def most_frequent(array):
+    counter = 0
+    num = array[0]
+     
+    for i in array:
+        curr_frequency = array.count(i)
+        if(curr_frequency> counter):
+            counter = curr_frequency
+            num = i
+ 
+    return num
 
-counter = 1
-
-capture = cv2.VideoCapture(-1)
-
-while not cv2.waitKey(20) & 0xFF == ord("q"):
-
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-    #while not cv2.waitKey(20) & 0xFF == ord("q"):
-    ret, frame_color = capture.read()
-    # LINUX:
-    # gray = cv2.cvtColor(frame_color, cv2.COLOR_BGR2GRAY) 
-
-    # WINDOWS
-    gray = cv2.cvtColor(frame_color)
-    faces = faceClassifier.detectMultiScale(gray)
-    faces_counter = 0
-    for x, y, w, h in faces:
-        cv2.rectangle(frame_color, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        faces_counter += 1
-    # cv2.imwrite("./teste2/2faces/teste3/frame%d.jpg" % counter, frame_color)
-    counter += 1
-
-    #cv2.imshow('color', frame_color)
-    #cv2.imshow('gray', gray)
-    print("Number the faces detected %d", faces_counter)
-
+def send_request_with_number_of_faces(number_of_faces):
     url= 'http://192.168.18.7:8000/api/central/sendNumberPeople'
     data = {
-        "numberPeople": faces_counter,
+        "numberPeople": number_of_faces,
     }
-
     response = requests.post(url=url, json=data)
-
+    print("Sending the request to the server...")
     if response.status_code >= 200 and response.status_code <=299:
         print(response.json())
     else:
         print(response)
 
-    time.sleep(60)
+def scan_most_occurrence_number_of_faces_by_time_window(time_interval, time_end):
+    
+    face_classifier = cv2.CascadeClassifier(XML_HAAR_CASCADE)
+    counter = 1
+    limit = int(time_end/time_interval)
+    faces_occurrences = []
+    capture = cv2.VideoCapture(-1)
+    
+    while not cv2.waitKey(20) & 0xFF == ord("q") and counter <= limit:
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+        ret, frame_color = capture.read()
+
+        gray = cv2.cvtColor(frame_color, cv2.COLOR_BGR2GRAY) # LINUX
+        # gray = cv2.cvtColor(frame_color)  # WINDOWS
+
+        faces = face_classifier.detectMultiScale(gray)
+        faces_counter = len(faces)
+        faces_occurrences.append(faces_counter)
+
+        print(str(counter) + " | Number the faces detected: " + str(faces_counter))
+        counter += 1
+        time.sleep(time_interval)
+
+    return most_frequent(faces_occurrences)
+
+def main():
+    while True:
+        number_of_faces = scan_most_occurrence_number_of_faces_by_time_window(1, 60)
+        send_request_with_number_of_faces(number_of_faces)
+
+if __name__ == "__main__":
+    main()
